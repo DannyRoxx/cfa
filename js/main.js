b2ea7e3f40e1f850bdcb97dfa35abe212c08322d
Daniell Skourup <dani042z@edu.easj.dk>
2019-05-23 11:31:39 +0200

    Website setup

--- js/main.js
+++ js/main.js
@@ -7,5 +7,37 @@
 		$("nav ul").toggle();
 	});
 	
+	$("#nav1").click(function() {
+		$("#menu1").toggle();
+		$("#menu2").hide();
+		$("#menu3").hide();
+		$("#menu4").hide();
+	});
+		 
+	$("#nav2").click(function() {
+		$("#menu1").hide();
+		$("#menu2").toggle();
+		$("#menu3").hide();
+		$("#menu4").hide();
+	});
+		 
+	$("#nav3").click(function() {
+		$("#menu1").hide();
+		$("#menu2").hide();
+		$("#menu3").toggle();
+		$("#menu4").hide();
+	});
+		 
+	$("#nav4").click(function() {
+		$("#menu1").hide();
+		$("#menu2").hide();
+		$("#menu3").hide();
+		$("#menu4").toggle();
+	});
 	
 });
+
+
+
+
+
